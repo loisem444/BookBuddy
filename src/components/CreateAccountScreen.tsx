c6854f74cc778e8eb3a3ddd3575c1676d70a793f
p4prabhu <p4prabhat01@gmail.com>
2023-11-10 00:05:10 -0500

    Login and signup screen

--- src/components/CreateAccountScreen.tsx
+++ src/components/CreateAccountScreen.tsx
@@ -1,28 +1,92 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { RootStackParamList } from '../../App';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'CreateAccount'>;
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, StyleSheet, Text, Alert } from 'react-native';
 
-const CreateAccountScreen: React.FC<Props> = ({ navigation }) => {
+const CreateAccountScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = () => {
+    if (email.trim() && password.trim()) {
+      navigation.navigate('Home');
+    } else {
+      Alert.alert('Error', 'Email and password cannot be blank.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Create account screen</Text>
-      <Button title="Go to Home" onPress={() => navigation.replace('Home')} />
+      <Text style={styles.headerText}>Create Your Account</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#666"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#666"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-export default CreateAccountScreen;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#e1f5fe', 
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 30,
+    color: '#3E4E5E', // Darker text for contrast
+  },
+  input: {
+    height: 50,
+    width: '90%',
+    borderColor: '#3E4E5E',
+    borderWidth: 1,
+    marginBottom: 15,
+    padding: 15,
+    borderRadius: 5,
+    backgroundColor: 'white', // White input background
+    fontSize: 16,
+  },
+  button: {
+    width: '90%',
+    backgroundColor: '#1E88E5', // A shade of blue for the button
+    padding: 15,
+    borderRadius: 5,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 10,
+    shadowColor: '#000', // Adding shadow for depth
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
 });
+
+export default CreateAccountScreen;
+
--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -1,29 +1,120 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { RootStackParamList } from '../../App';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+// LoginScreen.tsx
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, StyleSheet, Image, Text, KeyboardAvoidingView, Platform } from 'react-native';
 
-const LoginScreen: React.FC<Props> = ({ navigation }) => {
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // TODO: Implement real authentication logic here
+    if (email === 'test@example.com' && password === 'password123') {
+      navigation.navigate('Home');
+    } else {
+      // Handle login error, show an alert or set an error state
+      alert('Invalid credentials');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Login screen</Text>
-      <Button title="Go to Home" onPress={() => navigation.replace('Home')} />
-      <Button title="Go to Create account" onPress={() => navigation.navigate('CreateAccount')} />
-    </View>
+    <KeyboardAvoidingView 
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <View style={styles.loginContainer}>
+        <Image source={require('../../assets/logo.png')} style={styles.logo} />
+        <Text style={styles.appName}>Welcome to BookBuddy</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#666"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="#666"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('CreateAccount')}>
+          <Text style={styles.signUpText}>Don't have an account? <Text style={styles.signUp}>Sign Up</Text></Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
-
-export default LoginScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e1f5fe', 
+  },
+  loginContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    marginBottom: 20,
+  },
+  appName: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 30,
+    color: '#3E4E5E', // Darker text for contrast
+  },
+  input: {
+    height: 50,
+    width: '90%',
+    borderColor: '#3E4E5E',
+    borderWidth: 1,
+    marginBottom: 15,
+    padding: 15,
+    borderRadius: 5,
+    backgroundColor: 'white', // White input background
+    fontSize: 16,
+  },
+  button: {
+    width: '90%',
+    backgroundColor: '#1E88E5', // A shade of blue for the button
+    padding: 15,
+    borderRadius: 5,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 10,
+    shadowColor: '#000', // Adding shadow for depth
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  signUpText: {
+    marginTop: 20,
+    fontSize: 16,
+    color: '#3E4E5E',
+  },
+  signUp: {
+    color: '#1E88E5',
+    textDecorationLine: 'underline',
+  },
+  
 });
+
+export default LoginScreen;
